de5e2a095118af91463333bf13e888eb8ec35b99
root <root@localhost.localdomain>
2017-03-06 02:38:51 -0500

    I'm keeping track of threads in a really dumb way. I'm too tired to keep it up, obviously, so gonna finish up tomorrow

--- grab.py
+++ grab.py
@@ -6,29 +6,28 @@ num_threads = 2
 file_queue = Queue.Queue()
 threads = []
 
-# pass list of filenames
-def add_items(files,q):
-    for f in files:
-        q.put(f)
-
 # include trailing / - adds files in directory to queue
-def get_files(directory):
+def get_files(directory,q):
     path = directory + '*.json'
     files = glob.glob(path)
-    add_items(files,file_queue)
+    print files
+    count = 0
+    for f in files:
+        q.put(f)
+        count += 1
+    return count
 
 # pass 'file' - a JSON encoded filepath - print out needed values
 def parse_data(q):
-    while not q.empty():
+    with not q.empty():
         f = q.get()
-        print f
-        with open (f) as data_file:
-            data = json.load(data_file)
+    with open (f) as data_file:
+        data = json.load(data_file)
 
     info_j = data["additional_info"]["imports"]
     sha_j = data['sha256']
 
-    print "Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True)
+#    print "Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True)
     print "SHA256:\t", sha_j
 
 def build_threads(q,batch):
@@ -37,30 +36,34 @@ def build_threads(q,batch):
         worker.setDaemon(True)
         threads.append(worker)
 
-def start_threads():
-    count = 0
+def start_threads(count):
     for t in threads:
         print "Starting thread: ", count
         t.start()
-        count += 1
 
 def main():
-    # retrive files
-    get_files('files/')
-    # set up threads
-    build_threads(file_queue,num_threads)
-    # print thread count
-    print len(threads)
-    # Start threads
-    start_threads()
-    # join threads
-    for t in threads:
-        print t
-        t.join()
+    thread_count = 0
+    # get_files populates queue, returns an integer of amount of items put in queue
+    num_files = get_files('files/',file_queue)
+    while not file_queue.empty():
+        # set up threads
+        if num_threads <= num_files:
+            build_threads(file_queue,num_threads)
+        elif num_threads > num_files:
+            build_threads(file_queue,num_files)
+        # print thread count
+        print "total threads:", len(threads)
+        # Start threads
+        start_threads(thread_count)
+        # join threads
+        thread_count = 0
+        for t in threads:
+            t.join()
+            print "LOT:",len(threads)
+            threads.pop(thread_count)
+            thread_count += 1
+            print "LOT:",len(threads)
 
 
-
-
-file_queue.join()
 
 main()
