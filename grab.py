90a4d086bc0142d976e1b0f5041a26c57883ce0f
root <root@localhost.localdomain>
2017-03-05 23:50:29 -0500

    basic task done, need to make method and then queue it up

--- grab.py
+++ grab.py
@@ -3,4 +3,9 @@ import json
 with open ('test.json') as data_file:
     data = json.load(data_file)
 
+additional_info_j = data["additional_info"]["imports"]
+res1 = data["additional_info"]["imports"]
+sha_j = data['sha256']
 
+print "Additional_info imports:\n", json.dumps(res1, indent=2, sort_keys=True)
+print "SHA256:\t", sha_j
