c8540863ccd949e28cd65e5df3808820e6b8522e
root <root@localhost.localdomain>
2017-03-06 00:23:15 -0500

    just sending a copy up. still works, queue not in use yet

--- grab.py
+++ grab.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-import json
-with open ('test.json') as data_file:
+import json,thread,Queue
+
+# create a queue by reading in log files
+def create_queue(filename):
+    q = Queue.Queue()
+    q.put(filename)
+    
+def parse_data(file):    
+  with open (file) as data_file:
     data = json.load(data_file)
 
-additional_info_j = data["additional_info"]["imports"]
-res1 = data["additional_info"]["imports"]
-sha_j = data['sha256']
+  info_j = data["additional_info"]["imports"]
+  sha_j = data['sha256']
 
-print "Additional_info imports:\n", json.dumps(res1, indent=2, sort_keys=True)
-print "SHA256:\t", sha_j
+  print "Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True)
+  print "SHA256:\t", sha_j
+
+parse_data('files/test.json')
