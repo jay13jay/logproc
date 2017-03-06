a9b8ea232549a779a9f1b377db91849b4731871b
root <root@localhost.localdomain>
2017-03-06 01:06:13 -0500

    broke a few things, just want a check in point cause I'm using vim and keep making indent errors

--- grab.py
+++ grab.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
-import json,thread,Queue
+import json,thread,Queue,glob
 
-# create a queue by reading in log files
-def create_queue(filename):
-    q = Queue.Queue()
-    q.put(filename)
+num_threads = 2
+
+file_queue = Queue.Queue()
+# pass list of filenames
+def add_items(files,q):
+    for f in files:
+        q.put(f)
+
+# pass name of queue, returns item from queue
+def get_item(q):
+    if not q.empty():
+        return q.get()
     
-def parse_data(file):    
-  with open (file) as data_file:
-    data = json.load(data_file)
+# pass a directory containg json encoded files - include trailing /
+def get_files(directory):
+    path = directory + '*.json'
+    files = glob.glob(path)
+    add_items(files,file_queue)
 
-  info_j = data["additional_info"]["imports"]
-  sha_j = data['sha256']
+# pass 'file' - a JSON encoded filepath - print out needed values
+def parse_data(q):
+    while not q.empty():
+        f = get_item(q)
+        with open (f) as data_file:
+            data = json.load(data_file)
 
-  print "Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True)
-  print "SHA256:\t", sha_j
+    info_j = data["additional_info"]["imports"]
+    sha_j = data['sha256']
 
-parse_data('files/test.json')
+    print "Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True)
+    print "SHA256:\t", sha_j
+
+def build_threads(q,batch):
+    for i in range(batch):
+        worker = Thread(target=parse_data,args=(q,))
+        worker.setDaemon(True)
+        worker.start()
+
+
+build_threads(file_queue,num_threads)
