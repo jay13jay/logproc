46e1faa2be46919a844bd24f781683754bd92c72
Josh Hackney <jhackney@researchinnovations.com>
2017-04-21 15:22:00 -0400

    lists are mainly together, just need to do the final comparison and output

--- grab.py
+++ grab.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-import json,queue,glob
+import json,queue,glob,csv
 from threading import Thread
 
-
-num_threads = 2 
+file_dir = 'files/'
+outfile = 'output/outfile.csv'
+outdata = {}
+dll_list = []
+num_threads = 2
 file_queue = queue.Queue()
 threads = []
 
@@ -18,9 +21,13 @@ def get_files(directory,q):
     return count
 
 # pass 'file' - a JSON encoded filepath - print out needed values
-def parse_data(q):
+def parse_data(q,outdata):
     while not q.empty():
         f = q.get()
+        # output list - add elements to list, then append list to master list: [sha256[item1,item2[thing1,thing2],item3]]
+        output = {"name": "", "dll_list": []}
+        name_list = []
+
         try:
             with open (f) as data_file:
                 data = json.load(data_file)
@@ -28,19 +35,28 @@ def parse_data(q):
 
             info_j = data["additional_info"]["imports"]
             sha_j = data['sha256']
+            output.update({"name":sha_j})
+            output.update({"additional_info":info_j})
 
             #
-            # print "Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True)
-            print("SHA256:\t", sha_j)
+            output["name"] = sha_j
+            for key in info_j.keys():
+              dll_list.append(key)
+              output["dll_list"].append(key)
+
+            #print("Additional_info imports:\n", json.dumps(info_j, indent=2, sort_keys=True))
+            #print("SHA256:\t", sha_j)
+            print(output)
             q.task_done()
-        except:
+        except Exception as e:
             print("unable to open file!", f)
+            print("Error from system:\n",str(e))
             q.task_done()
 
 
-def build_threads(q,batch):
+def build_threads(q,outdata,batch):
     for i in range(batch):
-        worker = Thread(target=parse_data,args=(q,))
+        worker = Thread(target=parse_data,args=(q,outfile))
         worker.setDaemon(True)
         threads.append(worker)
 
@@ -53,20 +69,21 @@ def start_threads():
 def main():
     thread_count = 0
     # get_files populates queue, returns an integer of amount of items put in queue
-    num_files = get_files('files/',file_queue)
+    num_files = get_files(file_dir,file_queue)
     #while not file_queue.empty():
         # set up threads
     if num_threads <= num_files:
-        build_threads(file_queue,num_threads)
+        build_threads(file_queue,outdata,num_threads)
     elif num_threads > num_files:
-        build_threads(file_queue,num_files)
+        build_threads(file_queue,outfile,num_files)
     print("total threads:", len(threads))
     start_threads()
     # join threads
-    thread_count = 0
     for i in range(len(threads)):
         threads[i].join()
-            
+
+    sorted_dll = set(dll_list)
+    print(sorted_dll)
 
 
 print("Starting main loop")
